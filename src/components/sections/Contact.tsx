2da442bc795aae513990667f1ec33a12e24099da
Joowon Kim <32347532+0122wndnjs@users.noreply.github.com>
2026-02-08 00:48:38 +0900

    fixing issue

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -2,7 +2,7 @@ import { useEffect, useRef, useState } from "react";
 import { motion } from "framer-motion";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { FaTelegramPlane, FaGithub, FaLinkedin } from "react-icons/fa";
+import { FaTelegramPlane, FaGithub } from "react-icons/fa";
 import { SiGmail } from "react-icons/si";
 import { FiCreditCard } from "react-icons/fi";
 import BusinessCard from "./BusinessCard";
